cec0c963c0c215ebbbfee6df6a6920563e98aaf4
vladigr1 <vladigr1@gmail.com>
2019-08-01 10:38:33 +0300

    deletedExtra

--- exams/mem.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	int arr[2];
-	arr[0] =arr;
-	arr[1] = &arr[1];
-	printf("&arr[1] = %x &arr[0] = %x\n&arr[1] - &arr[0] = %d\n",&arr[1],&arr[0] ,&arr[1] - &arr[0] );
-	printf("arr[1] = %x arr[0] = %x\narr[1] - arr[0] = %d\n",arr[1],arr[0] ,arr[1] - arr[0] );
-}
